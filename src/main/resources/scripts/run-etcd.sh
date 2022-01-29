3e3268cbe1688c8fe0f66694b4514cbc51f95298
Alex Stockinger <alex@atomicjar.com>
2022-01-29 09:37:57 +0100

    Update license header

--- src/main/resources/scripts/run-etcd.sh
+++ src/main/resources/scripts/run-etcd.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-# Copyright 2020-2021 Alex Stockinger
+# Copyright 2020-2022 Alex Stockinger
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
