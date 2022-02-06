f0860b99cbd30bb7409a433fefca14faf98d1982
Alex Stockinger <mail@alexstockinger.de>
2022-02-06 22:11:53 +0100

    Add minimalistic kube-controller-manager fake

---
+++ src/main/java/com/dajudge/kindcontainer/client/model/reflection/ApiGroupVersion.java
@@ -0,0 +1,13 @@
+package com.dajudge.kindcontainer.client.model.reflection;
+
+public class ApiGroupVersion {
+    public void setGroupVersion(String groupVersion) {
+        this.groupVersion = groupVersion;
+    }
+
+    private String groupVersion;
+
+    public String getGroupVersion() {
+        return groupVersion;
+    }
+}
