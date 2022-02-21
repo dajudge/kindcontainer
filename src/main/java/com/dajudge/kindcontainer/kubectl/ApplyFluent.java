8fce2980c10a55785e3827f5115151d589f13478
Alex Stockinger <alex@atomicjar.com>
2022-02-21 08:29:13 +0100

    String interpolation for kubectl

--- src/main/java/com/dajudge/kindcontainer/kubectl/ApplyFluent.java
+++ src/main/java/com/dajudge/kindcontainer/kubectl/ApplyFluent.java
@@ -3,13 +3,22 @@ package com.dajudge.kindcontainer.kubectl;
 import com.dajudge.kindcontainer.BaseSidecarContainer.ExecInContainer;
 import com.dajudge.kindcontainer.BaseSidecarContainer.FileTarget;
 import com.dajudge.kindcontainer.exception.ExecutionException;
-import org.testcontainers.utility.MountableFile;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testcontainers.images.builder.Transferable;
+import org.testcontainers.shaded.org.apache.commons.io.IOUtils;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
+
+import static java.lang.Thread.currentThread;
+import static java.util.Arrays.asList;
+import static java.util.function.Function.identity;
 
 public class ApplyFluent<P> {
+    private static final Logger LOG = LoggerFactory.getLogger(ApplyFluent.class);
     private final ExecInContainer exec;
     private final FileTarget fileTarget;
     private final P parent;
@@ -29,15 +38,34 @@ public class ApplyFluent<P> {
     }
 
     public ApplyFluent<P> fileFromClasspath(final String resourceName) {
+        return fileFromClasspath(resourceName, identity());
+    }
+
+    public ApplyFluent<P> fileFromClasspath(final String resourceName, final Function<byte[], byte[]> transform) {
         final String path = "/tmp/classpath:" + resourceName
                 .replaceAll("_", "__")
                 .replaceAll("[^a-zA-Z0-9.]", "_");
         files.add(path);
-        preExecutionRunnables.add(() -> fileTarget.copyFileToContainer(
-                MountableFile.forClasspathResource(resourceName),
-                path
-        ));
+        final byte[] bytes = transform.apply(resourceToByteArray(resourceName));
+        final Transferable transferable = Transferable.of(bytes);
+        preExecutionRunnables.add(() -> fileTarget.copyFileToContainer(transferable, path));
         return this;
+    }
+
+    private byte[] resourceToByteArray(final String resourceName) {
+        final List<ClassLoader> classloaders = asList(
+                ApplyFluent.class.getClassLoader(),
+                currentThread().getContextClassLoader(),
+                ClassLoader.getSystemClassLoader()
+        );
+        for (final ClassLoader classloader : classloaders) {
+            try {
+                return IOUtils.resourceToByteArray(resourceName, ApplyFluent.class.getClassLoader());
+            } catch (final IOException e) {
+                LOG.debug("Failed to read classpath resource '{}' via classloader '{}'", resourceName, classloader, e);
+            }
+        }
+        throw new IllegalArgumentException("Unable to locate resource: " + resourceName);
     }
 
     public P run() throws IOException, ExecutionException, InterruptedException {
