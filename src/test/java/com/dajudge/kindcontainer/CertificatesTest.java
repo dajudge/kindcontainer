2c56e6c6c0266d983e75b4d0423f9f41567fff11
Alex Stockinger <alex@atomicjar.com>
2022-03-04 09:13:53 +0100

    Improve CA cert handling (#79)

--- src/test/java/com/dajudge/kindcontainer/CertificatesTest.java
+++ src/test/java/com/dajudge/kindcontainer/CertificatesTest.java
@@ -1,14 +1,20 @@
 package com.dajudge.kindcontainer;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.testcontainers.utility.MountableFile;
 
 import static com.dajudge.kindcontainer.TestUtils.stringResource;
 import static org.junit.Assert.assertTrue;
 
 public class CertificatesTest {
+    @Rule
+    public final KindContainer<?> k8s = new KindContainer<>()
+            .withCaCert(MountableFile.forClasspathResource("test.crt"));
+
     @Test
     public void adds_custom_certificate() {
-        final String allCerts = StaticContainers.kind().copyFileFromContainer(
+        final String allCerts = k8s.copyFileFromContainer(
                 "/etc/ssl/certs/ca-certificates.crt",
                 TestUtils::readString
         );
