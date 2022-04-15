3c0340a181f502cfcc7d815a6c979c0f98d6e147
Alex Stockinger <alex@atomicjar.com>
2022-04-15 08:59:21 +0200

    Add from(String) to kubectl.apply fluent

--- src/test/java/com/dajudge/kindcontainer/kubectl/KubectlApplyFluentTest.java
+++ src/test/java/com/dajudge/kindcontainer/kubectl/KubectlApplyFluentTest.java
@@ -1,39 +1,42 @@
 package com.dajudge.kindcontainer.kubectl;
 
 import com.dajudge.kindcontainer.ApiServerContainer;
-import com.dajudge.kindcontainer.K3sContainer;
-import com.dajudge.kindcontainer.KindContainer;
 import com.dajudge.kindcontainer.KubernetesContainer;
 import com.dajudge.kindcontainer.exception.ExecutionException;
 import io.fabric8.kubernetes.api.model.ConfigMap;
 import io.fabric8.kubernetes.client.DefaultKubernetesClient;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
+import org.testcontainers.utility.MountableFile;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.Objects;
 import java.util.UUID;
+import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
 import java.util.function.Consumer;
 import java.util.function.Function;
-import java.util.function.Supplier;
 
 import static io.fabric8.kubernetes.client.Config.fromKubeconfig;
 import static com.dajudge.kindcontainer.util.TestUtils.runWithClient;
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.util.concurrent.TimeUnit.SECONDS;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
+import static org.junit.Assert.*;
 import static org.testcontainers.shaded.org.awaitility.Awaitility.await;
+import static org.testcontainers.utility.MountableFile.forClasspathResource;
 
-public class KubectlFluentTest {
+public class KubectlApplyFluentTest {
 
     @Test
     public void multiple_invocations_work() {
+        /*
+        There was a bug where adding parameters via "fromXXX()" would not be cleared with the actual
+        invocation of the command via "run()".
+        This test reproduces this issue by applying 2 different manifests in separate "run()" invocations
+        while deleting some object of the first manifest in between. If the object pop up again due to
+        the unwanted repeated application of the first manifest we've captured the bug.
+         */
         withK8s(k8s -> k8s.withKubectl(kubectl -> {
+            // First manifest
             kubectl.apply
                     .fileFromClasspath(
                             "manifests/interpolation-configmap.yaml",
@@ -42,14 +45,27 @@ public class KubectlFluentTest {
                                     .replace("{{ name }}", "lolcats")
                                     .getBytes(UTF_8)
                     ).run();
-        }), k8s -> {
-            try (final DefaultKubernetesClient client = new DefaultKubernetesClient(fromKubeconfig(k8s.getKubeconfig()))) {
+        }), (k8s, client) -> {
+            try {
+                // Delete sth from the first manifest
                 client.inNamespace("default").configMaps().withName("lolcats").delete();
+                // Apply 2nd manifest
                 k8s.kubectl().apply.fileFromClasspath("manifests/serviceaccount1.yaml").run();
+                // Make sure the deleted object remains gone
                 assertNull(client.inNamespace("default").configMaps().withName("lolcats").get());
             } catch (final IOException | ExecutionException | InterruptedException e) {
                 throw new AssertionError(e);
             }
+        });
+    }
+
+    @Test
+    public void can_apply_from_string_param() {
+        withK8s(k8s -> k8s.withKubectl(kubectl -> {
+            kubectl.copyFileToContainer(forClasspathResource("manifests/serviceaccount1.yaml"), "/tmp/sa.yaml");
+            kubectl.apply.from("/tmp/sa.yaml").run();
+        }), (k8s, client) -> {
+            assertTrue(serviceAccountExists(client));
         });
     }
 
@@ -78,12 +94,18 @@ public class KubectlFluentTest {
         assertInterpolates(config, k8s -> k8s.getContainerInfo().getId());
     }
 
+    private boolean serviceAccountExists(final DefaultKubernetesClient client) {
+        return null != client.inNamespace("my-namespace").serviceAccounts()
+                .withName("my-service-account")
+                .get();
+    }
+
     private void assertInterpolates(
             final BiFunction<String, KubernetesContainer<?>, KubernetesContainer<?>> config,
             final Function<KubernetesContainer<?>, String> value
     ) {
         final String name = UUID.randomUUID().toString();
-        withK8s(k8s -> config.apply(name, k8s), k8s -> runWithClient(k8s, client -> {
+        withK8s(k8s -> config.apply(name, k8s), (k8s, client) -> {
             final ConfigMap configMap = await()
                     .timeout(10, SECONDS)
                     .until(
@@ -91,16 +113,18 @@ public class KubectlFluentTest {
                             Objects::nonNull
                     );
             assertEquals(value.apply(k8s), configMap.getData().get("my-key"));
-        }));
+        });
     }
 
     private void withK8s(
             final Function<KubernetesContainer<?>, KubernetesContainer<?>> config,
-            final Consumer<KubernetesContainer<?>> consumer
+            final BiConsumer<KubernetesContainer<?>, DefaultKubernetesClient> consumer
     ) {
         try (final KubernetesContainer<?> k8s = config.apply(new ApiServerContainer<>())) {
             k8s.start();
-            consumer.accept(k8s);
+            runWithClient(k8s, client -> {
+                consumer.accept(k8s, client);
+            });
         }
     }
 
--- src/test/java/com/dajudge/kindcontainer/kubectl/KubectlContainerTest.java
+++ src/test/java/com/dajudge/kindcontainer/kubectl/KubectlContainerTest.java
@@ -4,8 +4,6 @@ import com.dajudge.kindcontainer.ApiServerContainer;
 import com.dajudge.kindcontainer.K3sContainer;
 import com.dajudge.kindcontainer.KindContainer;
 import com.dajudge.kindcontainer.KubernetesContainer;
-import io.fabric8.kubernetes.client.DefaultKubernetesClient;
-import org.jetbrains.annotations.NotNull;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -14,7 +12,7 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.function.Supplier;
 
-import static io.fabric8.kubernetes.client.Config.fromKubeconfig;
+import static com.dajudge.kindcontainer.util.TestUtils.runWithClient;
 import static org.junit.Assert.assertNotNull;
 
 @RunWith(Parameterized.class)
@@ -34,15 +32,10 @@ public class KubectlContainerTest {
     public void kubectl_works() {
         try (final KubernetesContainer<?> k8s = createK8s()) {
             k8s.start();
-            try (final DefaultKubernetesClient client = createClient(k8s)) {
+            runWithClient(k8s, client -> {
                 assertNotNull(client.inNamespace("my-namespace").serviceAccounts().withName("my-service-account").get());
-            }
+            });
         }
-    }
-
-    @NotNull
-    private DefaultKubernetesClient createClient(KubernetesContainer<?> k8s) {
-        return new DefaultKubernetesClient(fromKubeconfig(k8s.getKubeconfig()));
     }
 
     private KubernetesContainer<?> createK8s() {
