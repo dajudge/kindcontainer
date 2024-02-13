b15291a75bd57a82ddd6774945ca789fe5bd4f63
Tomasz Michalak <tomek.michalak@gmail.com>
2024-02-13 09:36:11 +0100

    #264 Support for passing values as file in Helm (#265)

--- src/test/java/com/dajudge/kindcontainer/Helm3Test.java
+++ src/test/java/com/dajudge/kindcontainer/Helm3Test.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.TestFactory;
 
 import java.util.stream.Stream;
+import org.testcontainers.utility.MountableFile;
 
 import static com.dajudge.kindcontainer.util.ContainerVersionHelpers.allContainers;
 import static com.dajudge.kindcontainer.util.ContainerVersionHelpers.runWithK8s;
@@ -20,18 +21,20 @@ public class Helm3Test {
 
     private void assertCanInstallSomething(final KubernetesTestPackage<? extends KubernetesContainer<?>> testPkg) {
         runWithK8s(configureContainer(testPkg.newContainer()), k8s -> runWithClient(k8s, client -> {
-            assertFalse(client.apps().deployments().inNamespace("kubernetes-replicator").list().getItems().isEmpty());
+            assertFalse(client.apps().deployments().inNamespace("hello").list().getItems().isEmpty());
         }));
     }
 
     private KubernetesContainer<?> configureContainer(KubernetesContainer<?> container) {
         return container.withHelm3(helm -> {
-            helm.repo.add.run("mittwald", "https://helm.mittwald.de");
+            helm.copyFileToContainer(MountableFile.forClasspathResource("hello-values.yaml"), "/apps/values.yaml");
+            helm.repo.add.run("examples", "https://helm.github.io/examples");
             helm.repo.update.run();
             helm.install
-                    .namespace("kubernetes-replicator")
+                    .namespace("hello")
                     .createNamespace()
-                    .run("kubernetes-replicator", "mittwald/kubernetes-replicator");
+                    .values("/apps/values.yaml")
+                    .run("hello", "examples/hello-world");
         });
     }
 }
