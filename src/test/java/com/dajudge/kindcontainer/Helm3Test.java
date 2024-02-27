e710961f0b6fa699a0d026d3ea70b1ab7ee2dc9c
Alex Stockinger <mail@alexstockinger.de>
2024-02-27 09:47:00 +0100

    Add test to ensure version parameter is applied

--- src/test/java/com/dajudge/kindcontainer/Helm3Test.java
+++ src/test/java/com/dajudge/kindcontainer/Helm3Test.java
@@ -3,38 +3,57 @@ package com.dajudge.kindcontainer;
 import com.dajudge.kindcontainer.util.ContainerVersionHelpers.KubernetesTestPackage;
 import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.TestFactory;
+import org.testcontainers.utility.MountableFile;
 
 import java.util.stream.Stream;
-import org.testcontainers.utility.MountableFile;
 
 import static com.dajudge.kindcontainer.util.ContainerVersionHelpers.allContainers;
 import static com.dajudge.kindcontainer.util.ContainerVersionHelpers.runWithK8s;
 import static com.dajudge.kindcontainer.util.TestUtils.runWithClient;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class Helm3Test {
+
+    private static final String NAMESPACE = "hello";
 
     @TestFactory
     public Stream<DynamicTest> can_install_something() {
         return allContainers(this::assertCanInstallSomething);
     }
 
+    @TestFactory
+    public Stream<DynamicTest> assertInvalidVersionFails() {
+        return allContainers(this::assertInvalidVersionFails);
+    }
+
     private void assertCanInstallSomething(final KubernetesTestPackage<? extends KubernetesContainer<?>> testPkg) {
-        runWithK8s(configureContainer(testPkg.newContainer()), k8s -> runWithClient(k8s, client -> {
-            assertFalse(client.apps().deployments().inNamespace("hello").list().getItems().isEmpty());
+        assertCanInstallVersion(testPkg, "0.1.0");
+    }
+
+    private void assertInvalidVersionFails(final KubernetesTestPackage<? extends KubernetesContainer<?>> testPkg) {
+        assertThrows(RuntimeException.class, () -> assertCanInstallVersion(testPkg, "42.42.42"));
+    }
+
+    private void assertCanInstallVersion(
+            final KubernetesTestPackage<? extends KubernetesContainer<?>> testPkg,
+            final String version
+    ) {
+        runWithK8s(configureContainer(testPkg.newContainer(), version), k8s -> runWithClient(k8s, client -> {
+            assertFalse(client.apps().deployments().inNamespace(NAMESPACE).list().getItems().isEmpty());
         }));
     }
 
-    private KubernetesContainer<?> configureContainer(KubernetesContainer<?> container) {
+    private KubernetesContainer<?> configureContainer(final KubernetesContainer<?> container, final String version) {
         return container.withHelm3(helm -> {
             helm.copyFileToContainer(MountableFile.forClasspathResource("hello-values.yaml"), "/apps/values.yaml");
             helm.repo.add.run("examples", "https://helm.github.io/examples");
             helm.repo.update.run();
             helm.install
-                    .namespace("hello")
+                    .namespace(NAMESPACE)
                     .createNamespace()
                     .values("/apps/values.yaml")
-                    .version("0.1.0")
+                    .version(version)
                     .run("hello", "examples/hello-world");
         });
     }
