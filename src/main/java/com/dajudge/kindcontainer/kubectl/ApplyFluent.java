5c901769de13a6ca8aafe4538e24df00a6a1b42f
George Gastaldi <gegastaldi@gmail.com>
2025-12-01 10:01:50 -0300

    Bump to TestContainers 2.0.1 (#402)
    * Bump to TestContainers 2.0.1
    
    * Add missing org.junit.platform:junit-platform-launcher version

--- src/main/java/com/dajudge/kindcontainer/kubectl/ApplyFluent.java
+++ src/main/java/com/dajudge/kindcontainer/kubectl/ApplyFluent.java
@@ -6,7 +6,7 @@ import com.dajudge.kindcontainer.exception.ExecutionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testcontainers.images.builder.Transferable;
-import org.testcontainers.shaded.org.apache.commons.io.IOUtils;
+import org.apache.commons.io.IOUtils;
 
 import java.io.IOException;
 import java.util.ArrayList;
--- src/test/java/com/dajudge/kindcontainer/readme/junit4/SomeKindTest.java
+++ src/test/java/com/dajudge/kindcontainer/readme/junit4/SomeKindTest.java
@@ -1,23 +1,34 @@
 package com.dajudge.kindcontainer.readme.junit4;
 
 import com.dajudge.kindcontainer.KindContainer;
-import io.fabric8.kubernetes.client.DefaultKubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClient;
-import org.junit.ClassRule;
-import org.junit.Test;
+import io.fabric8.kubernetes.client.KubernetesClientBuilder;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 import static io.fabric8.kubernetes.client.Config.fromKubeconfig;
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SomeKindTest {
-    @ClassRule
     public static final KindContainer<?> KUBE = new KindContainer<>();
+
+    @BeforeAll
+    static void setUp() {
+        KUBE.start();
+    }
 
     @Test
     public void verify_node_is_present() {
         // Create a fabric8 client and use it!
-        try(final KubernetesClient client = new DefaultKubernetesClient(fromKubeconfig(KUBE.getKubeconfig()))) {
+        try (final KubernetesClient client = new KubernetesClientBuilder().withConfig(fromKubeconfig(KUBE.getKubeconfig())).build()) {
             assertEquals(1, client.nodes().list().getItems().size());
         }
     }
+
+    @AfterAll
+    static void tearDown() {
+        KUBE.stop();
+    }
+
 }
